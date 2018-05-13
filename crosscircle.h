c18e9e43fabae9adebafc271461ef5a1951ab79b
Piotr Przygoda <piotrprzygoda@MacBook-Air-Piotr.local>
2018-05-14 00:34:39 +0200

    Przydzielone deklaracje funkcji
    Utworzyłem przydzielone mi deklaracje funkcji dot. ruchu oraz wyboru poziomu trudności

--- crosscircle.h
+++ crosscircle.h
@@ -14,6 +14,15 @@ int sprawdzam_pion(int tryb, int tablica[3][3]);
 int sprawdzam_skosy(int tryb, int tablica[3][3]);
 int zagrozenie(int tryb, int tablica[3][3]);
 
+int poziom_trudnosci(void);
+
+void ruch_szympansa(int tablica[3][3]);
+void ruch_komputera(int tablica[3][3]);
+void ruch_pierwszy(int tablica [3][3]);
+int ruch_drugi(int tablica [3][3]);
+void ruch_trzeci(int scenariusz, int tablica [3][3]);
+int strategiczny_ruch_komputera(int ktory, int scenariusz, int tablica[3][3]);
+void ruch_gracz_1(int wybor_gracza1, int tablica[3][3]);
 
 
 
