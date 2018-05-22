2791226c5a536174f21b3fbb2cb863575cdc35a6
Blaxu <aghinpg@gmail.com>
2018-05-22 22:19:31 +0200

    Funkcje

--- Gra 2/saper.cpp
+++ Gra 2/saper.cpp
@@ -9,3 +9,17 @@
 #define strzalka_gora 0x26
 
 #define enter 0x0D
+
+
+using namespace std;
+
+struct pole
+{
+    int wartosc;
+    bool odkryte;                                       
+};
+
+pole plansza[10][10];
+
+int poz_x = 0, poz_y = 0, o_poz_x = 1, o_poz_y = 1;
+int koniec = 0;
