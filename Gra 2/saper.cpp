6bfcaf58412ab5c9d1adcf9b1459f9f7b0bc872f
Porbi <38583796+Porbi@users.noreply.github.com>
2018-05-22 20:26:51 +0200

    Implementacja bibliotek

---
+++ Gra 2/saper.cpp
@@ -0,0 +1,11 @@
+#include <conio.h>
+#include <windows.h>
+#include <iostream>
+#include <time.h>
+
+#define strzalka_lewo 0x25
+#define strzalka_prawo 0x27
+#define strzalka_dol 0x28
+#define strzalka_gora 0x26
+
+#define enter 0x0D
