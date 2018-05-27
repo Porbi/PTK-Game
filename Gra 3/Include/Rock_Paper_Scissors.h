164df58d32f6da2c8c41f036f70806c1cccfc43a
cinekM <mh.inpg.agh@gmail.com>
2018-05-27 17:36:31 +0200

    output/input

--- Gra 3/Include/Rock_Paper_Scissors.h
+++ Gra 3/Include/Rock_Paper_Scissors.h
@@ -15,5 +15,32 @@ private:
 
 std::string choose_round_winner(Player& human, Player& computer);
 
+class Output {
+
+public:
+    void show_start() const;
+    void show_choices() const;
+    void show_chosen(int player_choice, int computer_choice) const;
+    void show_score(std::size_t player_score, std::size_t computer_score) const;
+    void show_round_winner(std::string r_winner) const;
+    void show_final_score(std::size_t human_score, std::size_t computer_score) const;
+    ~Output() = default;
+};
+
+class Input {
+
+public:
+    void ask_choice(Player& player);
+    void ask_round(int& round_count);
+    ~Input() = default;
+};
+
+
+
+
+
+
+
+#endif //RPS_ROCK_PAPER_SCISSORS_H
 
 
