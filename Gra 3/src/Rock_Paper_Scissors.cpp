fd74f188e7f28491d83d017d9855d6ce904355a2
Blaxu <aghinpg@gmail.com>
2018-05-28 23:41:20 +0200

    Kod i sprawozdanie

--- Gra 3/src/Rock_Paper_Scissors.cpp
+++ Gra 3/src/Rock_Paper_Scissors.cpp
@@ -1,3 +1,32 @@
+#include "../include/Rock_Paper_Scissors.h"
+
+void Output::show_start() const {
+    std::string name = "[ROCK PAPER SCISSORS GAME]";
+    std::cout<< name << std::endl;
+    pause(1);
+    std::cout<< "\nRULES:\n";
+    pause(1);
+    std::cout<< "- Rock beats Scissors\n";
+    pause(1);
+    std::cout<< "- Paper beats Rock\n";
+    pause(1);
+    std::cout<< "- Scissors beat Paper\n\n\n";
+    pause(5);
+}
+
+void Output::show_choices() const {
+    std::cout << "       |CHOICES|" << std::endl;
+    pause(1);
+    std::cout << "\n1. ROCK, ";
+    std::cout << "2. PAPER, ";
+    std::cout << "3. SCISSORS" << std::endl;
+    pause(1);
+    std::cout << "\nOR" << std::endl;
+    pause(1);
+    std::cout << "4. Quit Game" << std::endl;
+    pause(2);
+}
+
 void Output::show_score(std::size_t player_score, std::size_t computer_score) const {
     std::cout << "Score:\n" << std::endl;
     pause(1);
