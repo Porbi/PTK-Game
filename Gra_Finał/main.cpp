53ac47e2b3e28ca899c883bc0553d4d070a43d35
cinekM <mh.inpg.agh@gmail.com>
2018-06-05 22:11:50 +0200

    main

--- Gra_Finał/main.cpp
+++ Gra_Finał/main.cpp
@@ -39,3 +39,55 @@
                          "Druga z nich odpowiada wspolrzednej poziomej(wiersz)");
             int tablica[3][3];
             czysc_tablice(tablica);
+            switch (tryb_gry()) {
+                case 1: {
+                    wypisz_tablice(tablica);
+                    int wybor_gracza1;
+                    int wybor_gracza2;
+                    switch (wczytaj_symbol_gracza()) {
+                        case WARTOSC_X:
+                            wybor_gracza1 = 1;
+                            wybor_gracza2 = 2;
+                            break;
+                        case WARTOSC_O:
+                            wybor_gracza1 = 2;
+                            wybor_gracza2 = 1;
+                            break;
+                    }
+                    for (int i = 0; i < 4; i++) {
+                        ruch_gracz_1(wybor_gracza1, tablica);
+                        ruch_gracz_2(wybor_gracza2, tablica);
+                    }
+                    ruch_gracz_1(wybor_gracza1, tablica);
+                    puts("\n\nRemis\n\n");
+                    return 0;
+                }
+                case 2:
+                    switch (poziom_trudnosci()) {
+                        case TRUDNOSC_SZYMPANS: {
+                            puts("/n");
+                            wypisz_tablice(tablica);
+                            puts("Komputer - X, gracz - Y\n");
+                            for (int i = 0; i < 4; i++) {
+                                ruch_szympansa(tablica);
+                                wypisz_tablice(tablica);
+                                puts("\n");
+                                if (sprawdz_wygrana(tablica) == 1) {
+                                    wypisz_tablice(tablica);
+                                    puts("\n");
+                                    return 0;
+                                }
+                                ruch_gracz_1(2, tablica);
+                            }
+                            ruch_gracz_1(2, tablica);
+                            puts("\n\nRemis\n\n");
+                            return 0;
+                        }
+                    }
+            }
+        case 3 :
+            std::cout << "See You Later" << std::endl;
+            pause(1);
+            exit(0);
+    }
+}
